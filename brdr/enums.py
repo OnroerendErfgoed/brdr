52fc6f41b46d023ffd5edd7613dbe8d6bb5eff60
dieuska <karel.dieussaert@geosolutions.be>
2024-05-17 17:27:38 +0200

    cleanup od_strategies
    cleanup od_strategies

--- brdr/enums.py
+++ brdr/enums.py
@@ -11,24 +11,27 @@ class OpenbaarDomeinStrategy(IntEnum):
     *   -1 (EXCLUDE): Completely exclude everything that is not on the reference layer
     *   0 (AS_IS): All parts that are not covered by the reference layer are added to the
         resulting geometry AS IS
-    *   1 (SNAP_ONE_SIDED): Everything that falls within the relevant distance over the
+    *   1 (SNAP_SINGLE_SIDE): Everything that falls within the relevant distance over the
         plot boundary is snapped to the plot. The outer boundary is not used.
-    *   2 (SNAP_BOTH_SIDED): Everything that falls within the relevant distance over the
+    *   2 (SNAP_ALL_SIDE): Everything that falls within the relevant distance over the
         plot boundary is snapped to the plot. The outer boundary is used.
-    *   3 (SNAP_BIG_AREA): integrates entire Openbaar Domein within buffered original
+    *   3 (SNAP_FULL_AREA_SINGLE_SIDE): integrates entire Openbaar Domein within buffered original
         geometry
-    *   4 (SNAP_FULL_AREA): integrates all OD within inside-buffered original geometry.
-        The first part is a copy of SNAP_BOTH_SIDED
-    *   5 (SNAP_FULL_AREA_BOTH_SIDED): integrates all OD within outside-buffered original
+    *   4 (SNAP_FULL_AREA_ALL_SIDE): integrates all OD within inside-buffered original geometry.
+        The first part is a copy of SNAP_ALL_SIDE
+    *   5 (SNAP_SINGLE_SIDE_VARIANT_1): implementation variant 1 of strategy SNAP_SINGLE_SIDE
+    *   6 (SNAP_SINGLE_SIDE_VARIANT_2): implementation variant 2 of strategy SNAP_SINGLE_SIDE
 
     """
 
     EXCLUDE = -1
     AS_IS = 0
-    SNAP_ONE_SIDED = 1
-    SNAP_BOTH_SIDED = 2
-    SNAP_FULL_AREA_ONE_SIDED = 3
-    SNAP_FULL_AREA_BOTH_SIDED = 4
+    SNAP_SINGLE_SIDE = 1
+    SNAP_ALL_SIDE = 2
+    SNAP_FULL_AREA_SINGLE_SIDE = 3
+    SNAP_FULL_AREA_ALL_SIDE = 4
+    SNAP_SINGLE_SIDE_VARIANT_1 = 5
+    SNAP_SINGLE_SIDE_VARIANT_2 = 6
 
 
 class GRBType(str, Enum):
