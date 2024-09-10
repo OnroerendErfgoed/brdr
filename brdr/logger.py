c111e44be6e760b944fb2c0979bd816f5e1aee1a
Emrys Roef <emrys.roef@geosolutions.be>
2024-09-10 12:08:10 +0200

    fix logging

--- brdr/logger.py
+++ brdr/logger.py
@@ -1,9 +1,15 @@
 import logging
+import sys
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(message)s", datefmt="%d-%b-%y %H:%M:%S"
+LOGGER = logging.getLogger("BRDR")
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    fmt="%(asctime)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
 )
-
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
 
 class Logger:
     def __init__(self, feedback=None):
@@ -13,16 +19,16 @@ class Logger:
         if self.feedback is not None:
             # self.feedback.pushInfo(text)
             return
-        logging.debug(text)
+        LOGGER.debug(text)
 
     def feedback_info(self, text):
         if self.feedback is not None:
             self.feedback.pushInfo(text)
             return
-        logging.info(text)
+        LOGGER.info(text)
 
     def feedback_warning(self, text):
         if self.feedback is not None:
             self.feedback.pushInfo(text)
             return
-        logging.warning(text)
+        LOGGER.warning(text)
