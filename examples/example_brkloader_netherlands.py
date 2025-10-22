cb960dde4a2a89dd602e55870733ae2d606efa24
dieuska <karel.dieussaert@geosolutions.be>
2025-10-22 18:12:25 +0200

    moved be and nl loader

--- examples/example_brkloader_netherlands.py
+++ examples/example_brkloader_netherlands.py
@@ -1,7 +1,7 @@
 from brdr.aligner import Aligner
 from brdr.geometry_utils import geom_from_wkt
 from brdr.loader import DictLoader
-from brdr.nl import BRKLoader, BRKType
+from brdr.loaders.nl import BRKLoader, BRKType
 from brdr.osm import OSMLoader
 
 if __name__ == "__main__":
